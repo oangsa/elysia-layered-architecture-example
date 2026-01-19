c4e4cce6b8d0bce1da6ba176e2929364d1b6d69b
Suthang Sukrueangkun <sukruangkul.aongsa@gmail.com>
2026-01-19 22:38:21 +0700

    Update NotFoundException.ts

--- src/Entities/Exceptions/NotFoundException.ts
+++ src/Entities/Exceptions/NotFoundException.ts
@@ -1,20 +1,3 @@
-// using System.Runtime.Serialization;
-
-// namespace AXONSAPI.Entities.Exceptions
-// {
-//     public abstract class NotFoundException : Exception
-//     {
-//         protected NotFoundException(string message) : base(message)
-//         { }
-//         protected NotFoundException(SerializationInfo info, StreamingContext context)
-//         #pragma warning disable SYSLIB0051 // Type or member is obsolete
-//       : base(info, context)
-//         #pragma warning restore SYSLIB0051 // Type or member is obsolete
-//         {
-//         }
-//     }
-// }
-
 export abstract class NotFoundException extends Error
 {
     protected constructor(message: string)
